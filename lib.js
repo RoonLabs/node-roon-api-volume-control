e3e012b2376b87a5af82c10cc5cc9f4a53e2907a
Danny Dulai <danny@ishiboo.com>
2016-08-25 10:27:20 -0400

    key -> control_key

--- lib.js
+++ lib.js
@@ -19,11 +19,11 @@ function RoonApiVolumeControl(roon, opts) {
                 req.send_complete("Success", { controls: this._objs.reduce((p,e) => p.push(e.state) && p, []) });
             },
             set_volume: (req) => {
-                var d = this._objs[req.body.key];
+                var d = this._objs[req.body.control_key];
                 d.set_volume(req, req.body.mode, req.body.value);
             },
             set_mute: (req) => {
-                var d = this._objs[req.body.key];
+                var d = this._objs[req.body.control_key];
                 d.set_mute(req, req.body.action);
             }
         }
@@ -33,13 +33,13 @@ function RoonApiVolumeControl(roon, opts) {
 }
 
 RoonApiVolumeControl.prototype.new_device = function(o) {
-    o.state.key = this._id++;
-    this._objs[o.state.key] = o;
+    o.state.control_key = this._id++;
+    this._objs[o.state.control_key] = o;
     this._svc.send_continue_all('subscribe_controls', "Changed", { controls_added: [ o.state ] });
     return {
         destroy: () => {
-            this._svc.send_continue_all('subscribe_controls', "Changed", { controls_removed: [ o.key ] });
-            delete(this._objs[o.key]);
+            this._svc.send_continue_all('subscribe_controls', "Changed", { controls_removed: [ o.control_key ] });
+            delete(this._objs[o.control_key]);
         },
         update_state: (state) => {
             for (let x in state) if (o.state[x] !== state[x]) o.state[x] = state[x];

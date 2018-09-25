cea31abb11801e3a68f881c8f3ec54b10635a655
Danny Dulai <danny@roonlabs.com>
2018-09-25 18:22:32 -0400

    this is buggy code, because control_key should be unique and tied to the output device.
    we should always require it to be set. this change allows it to be set, but provides a simple/stupid default if it isnt set. this shouldnt break anyone with 1 device, but will definitely break people with more than one.

--- lib.js
+++ lib.js
@@ -1,8 +1,7 @@
 "use strict";
 
 function RoonApiVolumeControl(roon, opts) {
-    this._objs = []
-    this._id = 1;
+    this._objs = {};
 
     this._svc = roon.register_service("com.roonlabs.volumecontrol:1", {
         subscriptions: [
@@ -10,13 +9,13 @@ function RoonApiVolumeControl(roon, opts) {
                 subscribe_name:   "subscribe_controls",
                 unsubscribe_name: "unsubscribe_controls",
                 start: (req) => {
-                    req.send_continue("Subscribed", { controls: this._objs.reduce((p,e) => p.push(e.state) && p, []) });
+                    req.send_continue("Subscribed", { controls: Object.values(this._objs).reduce((p,e) => p.push(e.state) && p, []) });
                 }
             }
         ],
         methods: {
             get_all: (req) => {
-                req.send_complete("Success", { controls: this._objs.reduce((p,e) => p.push(e.state) && p, []) });
+                req.send_complete("Success", { controls: Object.values(this._objs).reduce((p,e) => p.push(e.state) && p, []) });
             },
             set_volume: (req) => {
                 var d = this._objs[req.body.control_key];
@@ -33,19 +32,23 @@ function RoonApiVolumeControl(roon, opts) {
 }
 
 RoonApiVolumeControl.prototype.new_device = function(o) {
-    o.state.control_key = this._id++;
+    o.state.control_key = o.state.control_key || "1";
+
+    if (this._objs[o.state.control_key])
+        throw new Error('Must set control key to unique id');
+
     this._objs[o.state.control_key] = o;
     this._svc.send_continue_all('subscribe_controls', "Changed", { controls_added: [ o.state ] });
     return {
         destroy: () => {
-            this._svc.send_continue_all('subscribe_controls', "Changed", { controls_removed: [ o.control_key ] });
-            delete(this._objs[o.control_key]);
+            this._svc.send_continue_all('subscribe_controls', "Changed", { controls_removed: [ o.state.control_key ] });
+            delete(this._objs[o.state.control_key]);
         },
         update_state: (state) => {
             for (let x in state) if (o.state[x] !== state[x]) o.state[x] = state[x];
             this._svc.send_continue_all('subscribe_controls', "Changed", { controls_changed: [ o.state ] });
         }
-    }
+    };
 };
 
 exports = module.exports = RoonApiVolumeControl;
